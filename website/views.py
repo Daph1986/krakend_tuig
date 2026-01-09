19e6738468a364bb0b9d907f9fa6d9cbd741389c
Daph1986 <daphne.heimgartner@gmail.com>
2026-01-09 21:25:54 +0100

    setup env and adjust settings

--- website/views.py
+++ website/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    # als je nog geen Page met slug 'home' hebt, gebruik een tijdelijke placeholder
+    return render(request, 'website/page.html', {'page': {'title': 'Welkom', 'body': 'Dit is de homepage'}})
