94594aaeb5eaa0e9825ef36f2b80c64afe05e2d0
Daph1986 <daphne.heimgartner@gmail.com>
2026-01-19 11:38:50 +0100

    Set up zing mee page

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from . import views
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path(_('zing_mee/'), views.zing_mee, name='zing_mee'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def zing_mee(request):
+    return render(request, 'zing_mee.html')
