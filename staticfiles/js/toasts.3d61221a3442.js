59e26a5fb80d2e532953f3450c942b150d1c5a71
Daph1986 <daphne.heimgartner@gmail.com>
2026-02-06 15:33:35 +0100

    Ignore collected static files and stop tracking them

--- staticfiles/js/toasts.3d61221a3442.js
+++
@@ -1,6 +0,0 @@
-// ----------------------- Bootstrap toast initialize  ----------------------- //
-
-let toastMessage = [].slice.call(document.querySelectorAll('.toast'));
-let message = toastMessage.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl).show();
-});
