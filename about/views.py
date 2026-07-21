86898e27dabcee87972bf885ebd07a3b56d48253
Daph1986 <daphne.heimgartner@gmail.com>
2026-07-21 15:10:52 +0200

    Add selfcount roles

--- about/views.py
+++ about/views.py
@@ -4,11 +4,41 @@ from django.shortcuts import render, redirect
 
 from .models import AboutIntro
 from .forms import AboutIntroForm
+from accounts.models import MemberProfile
 
 
 def about_page(request):
     intro, _ = AboutIntro.objects.get_or_create(pk=1)
-    return render(request, 'about.html', {'intro': intro})
+    active_members = MemberProfile.objects.filter(is_active=True, user__is_active=True)
+
+    # Tel exact op basis van de rollen 'muzikant' en 'zanger'
+    muzikanten_count = active_members.filter(role__iexact='muzikant').count()
+    zangers_count = active_members.filter(role__iexact='zanger').count()
+
+    # Vervang {muzikanten} en {zangers} veilig in de teksten
+    def format_paragraph(text):
+        if not text:
+            return text
+        return text.replace('{muzikanten}', str(muzikanten_count)).replace('{zangers}', str(zangers_count))
+
+    processed_paragraphs = {
+        'paragraph_1': format_paragraph(intro.paragraph_1),
+        'paragraph_2': format_paragraph(intro.paragraph_2),
+        'paragraph_3': format_paragraph(intro.paragraph_3),
+        'paragraph_4': format_paragraph(intro.paragraph_4),
+        'paragraph_5': format_paragraph(intro.paragraph_5),
+        'paragraph_6': format_paragraph(intro.paragraph_6),
+        'paragraph_7': format_paragraph(intro.paragraph_7),
+    }
+
+    context = {
+        'intro': intro,
+        'paragraphs': processed_paragraphs,
+        'muzikanten_count': muzikanten_count,
+        'zangers_count': zangers_count,
+    }
+
+    return render(request, 'about.html', context)
 
 
 @staff_member_required
